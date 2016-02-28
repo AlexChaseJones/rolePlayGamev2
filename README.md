f4ab8d31f3abb0a886f62b8535a89f95b8440d99
Alexander <alexchasejones@icloud.com>
2016-02-28 14:19:09 -0500

    no time

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # rolePlayGamev2
 
 second attempt at building the game, first attempt was almost there, but It failed and it was a result of a lot of a horribly designed foundation that I couldn't go back and fix.
+
+----notes for me----
+Role Play Game.
+
+The game will be divided up into 3 catagories: click events, functions, and condition checks.
+
+Lets run through how the game will work, from beginning to end.
+
+When the page is loaded, I will write to the ‘characters’ html element all four characters. there will be a click event on these characters, and when it is activated it will store which character the user clicked into a variable. the ‘characters’ html element will then be deleted, and all the characters will be placed in the ‘environment’ html element. the player character will have different styling than the enemy characters, and each enemy character will have its on ‘id’ and data-index number. Next, when an enemy is clicked on, it is turned into a defender, once an enemy is turned into a defender, all other enemies need to be made inactive by some means. A button will now pop up that says attack. When this button is clicked, it checks the attack of the current player and subtracts it from the defenders health, and subtracts the defenders counter attack from the attackers health. a conditional statement is ran that checks if the enemies health is below zero. if it is, then that enemy is removed, and the other characters are re-activated. There should also be a check to see if the players health has dropped below zero, and if so the game is reset. When the other enemies are added back onto the page, the event listener will be reactivated that looks for an enemy to be clicked. this will continue until either all enemies are deleted, or players health reaches zero.
+
+EVENT LISTENERS NEEDED
+
+A listener on the characters for choosing a player.
+
+A listener on enemies for choosing a defender,
+
+A listener on attack button for attacking defender.
+——
+The character listener should do a couple things: Store the player, store enemies, and make and append the divs to the environment. It should add classes to the enemies that the next event listener can listen for.
+
+The defender listener should do a couple things: Remove all traces of the listener so that another defender cannot be chosen. store the enemy clicked on in a variable so that it can be worked on. Add an attack button.
+
+The attack button listener should do a couple things: It should activate a function that takes in player and defender, subtracts whats needed, and then updates the health. The conditional statements should be in here as well. When the button is clicked and the attack function is run, it should check the health of the player and the defender and decide what to do.
+
+CONDITIONAL STATEMENTS
+
+I will need one conditional statement to check if the players health is 0, or if the defenders health is 0. 
+
+I will also need to check if there are no enemies left to battle. I can do this by storing all characters in an array, and removing them when they are picked(player) or killed as a defender, then checking the length of the array. This statement should be checked in the attack button listener.
--- script/javascript.js
+++ script/javascript.js
@@ -1,67 +1,99 @@
+// $(document).ready(function(){
 
-	// Character objects
-var starfox = {
-	name: "Star Fox",
-	indexNum: 0,
-	picture: "images/starfox.png",
-	health: 150,
-	baseAttack: 4,
-	currentAttack: 0,
-	attack: function(){
-		this.currentAttack += 4;
+var characterNames = ['starfox','mario','link','donkeyKong'];
+//Character Builds.
+	characters = {
+	starfox: {
+		name: "Star Fox",
+		indexNum: 0,
+		picture: "images/starfox.png",
+		health: 150,
+		baseAttack: 4,
+		currentAttack: 0,
+		attack: function(){
+			this.currentAttack += 4;
+		},
+		counter: 24,
 	},
-	counter: 24,
+
+	mario: {
+		name: "Mario",
+		indexNum: 1,
+		picture: "images/mario.png",
+		health: 120,
+		baseAttack: 6,
+		currentAttack: 0,
+		attack: function(){
+			this.currentAttack += 6;
+		},
+		counter: 30,
+	},
+
+	link: {
+		name: "Link",
+		indexNum: 2,
+		picture: "images/link.png",
+		health: 150,
+		baseAttack: 5,
+		currentAttack: 0,
+		attack: function(){
+			this.currentAttack += 5;
+		},
+		counter: 20,
+	},
+
+	donkeyKong: {
+		name: "Donkey Kong",
+		indexNum: 3,
+		picture: "images/donkeyKong.png",
+		health: 190,
+		baseAttack: 3,	
+		currentAttack: 0,
+		attack: function(){
+			this.currentAttack += 3;
+		},
+		counter: 35, 
+	}
+};
+//Adds functions that build HTML code to all characters.
+for (var i = 0; i < characterNames.length; i++) {
+	var target = characterNames[i];
+
+	function choiceView(){
+	return "<h1>" + this.name + "</h1><img src='" + this.picture + "' /><h2>Health Points: <br/>" + this.health + "</h2><h3>Base Attack: " + this.baseAttack + "</h3>"
+	};
+
+	function battleView(){
+	return "<div class='headBar'><h1>" + this.name + "</h1><h2>" + this.health + "HP</h2><div class='clearfix'></div></div><img src='" + this.picture + "' />" 
+	};	
+
+	characters[target].choiceCode = choiceView;
+	characters[target].battleCode = battleView;
 };
 
-var mario = {
-	name: "Mario",
-	indexNum: 1,
-	picture: "images/mario.png",
-	health: 120,
-	baseAttack: 6,
-	currentAttack: 0,
-	attack: function(){
-		this.currentAttack += 6;
-	},
-	counter: 30,
+//Initial setup, puts Characters into #characters.
+
+for (var i = 0; i <characterNames.length; i++) {
+	var target = characterNames[i];
+
+	var chooseplayerDiv = $('<div class="choice">');
+	$(chooseplayerDiv).html(characters[target].choiceCode());
+	$(chooseplayerDiv).attr('data-index', characterNames[i]);
+	$(chooseplayerDiv).appendTo('#characters');
 };
 
-var link = {
-	name: "Link",
-	indexNum: 2,
-	picture: "images/link.png",
-	health: 150,
-	baseAttack: 5,
-	currentAttack: 0,
-	attack: function(){
-		this.currentAttack += 5;
-	},
-	counter: 20,
+$('.choice').on('click', enemyEnvironmentBuild);
+
+function enemyEnvironmentBuild(){
+	
 };
 
-var donkeyKong = {
-	name: "Donkey Kong",
-	indexNum: 3,
-	picture: "images/donkeyKong.png",
-	health: 190,
-	baseAttack: 3,	
-	currentAttack: 0,
-	attack: function(){
-		this.currentAttack += 3;
-	},
-	counter: 35, 
-};
 
-var charArray = [starfox, mario, link, donkeyKong];
+// })
 
-function start(){
-	for (var i = 0; i < charArray.length; i++) {
-		var choiceView = function(){
-		return "<h1>" + this.name + "</h1><img src='" + this.picture + "' /><h2>Health Points: <br/>" + this.health + "</h2><h3>Base Attack: " + this.baseAttack + "</h3>"
-		};
-		charArray[i].choiceCode = choiceView;
 
-		var battleView = function(){
-		return "<div class='headBar'><h1>" + this.name + "</h1><h2>" + this.health + "HP</h2><div class='clearfix'></div></div><img src='" + this.picture + "' />" 
-		};	
-		charArray[i].battleCode = battleView;
+
+
+
+
+
